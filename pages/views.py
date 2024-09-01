a0902c9cacd586e383e69188d66ae2836c9a7ee1
RandomNimrod69 <olaroiuraducu@gmail.com>
2024-09-01 17:56:23 +0300

    lecture 32

--- pages/views.py
+++ pages/views.py
@@ -5,10 +5,12 @@ from cars.models import Car  # Correct import of the Car model
 
 def home(request):
     featured_cars = Car.objects.order_by('-data_adaugare').filter(is_featured=True)
+    all_cars = Car.objects.order_by('-data_adaugare')  # Use 'data_adaugare' here instead of 'created_date'
     data = {
-        'featured_cars': featured_cars
+        'featured_cars': featured_cars,
+        'all_cars': all_cars,
     }
-    return render(request, 'pages/home.html', data)
+    return render(request, 'home.html', data)
 
 def about(request):
     return render(request, 'pages/about.html')
